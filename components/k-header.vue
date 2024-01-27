81737e4755384d1108c64b72e69c984b873d14a4
jordanfurr <ejfurr@umich.edu>
2024-01-27 12:27:32 -0500

    stuff

--- components/k-header.vue
+++ components/k-header.vue
@@ -3,11 +3,9 @@
         <div class="title">
             <h1 class="logo">KATRINACITY</h1>
         </div>
-        <div class="nav">
-            <div class="menu">
-                <Nuxtlink to="/art">art</Nuxtlink>
-                <Nuxtlink to="/about">about</Nuxtlink>
-            </div>
+        <div class="menu">
+            <p><Nuxtlink to="~/pages/art">art</Nuxtlink></p>
+            <p><Nuxtlink to="~/pages/about">about</Nuxtlink></p>
         </div>
     </div>
 </template>
@@ -20,10 +18,21 @@
 .wrapper{ 
     background-color: #ff2830;
     display: flex;
+    flex-direction: row;
     align-items: center;
 }
+.title {
+    margin-left: 2em;
+}
 .menu{
-    color: white;
+    display: flex;
+    flex-direction: row;
     text-decoration: none;
+}
+.menu p {
+    padding-left: 2em;
+}
+.menu p:hover {
+    color: white;
 }
 </style>
