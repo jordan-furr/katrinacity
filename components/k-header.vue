6bcd46434549e7761a262a855fb02d7ff9f7629f
jordanfurr <ejfurr@umich.edu>
2024-01-31 10:09:32 -0500

    left-nav

--- components/k-header.vue
+++ components/k-header.vue
@@ -2,13 +2,13 @@
     <nav>
         <div class="wrapper">
             <div class="title">
-                <h1 class="logo"><NuxtLink to="/">KATRINACITY</NuxtLink></h1>
+                <h1 class = "logo"><NuxtLink class="logo-text" to="/">KATRINACITY</NuxtLink></h1>
             </div>
             <div class="menu">
-                <p class="menu-item"><NuxtLink to="/art">art</NuxtLink></p>
-                <p class="menu-item"><NuxtLink to="/about">about</NuxtLink></p>
-                <p class="menu-item"><NuxtLink to="/work">work</NuxtLink></p>
-                <p class="menu-item"><NuxtLink to="/art">art</NuxtLink></p>
+                <p class="menu-item"><NuxtLink class="menu-text" to="/art">art</NuxtLink></p>
+                <p class="menu-item"><NuxtLink class="menu-text" to="/about">about</NuxtLink></p>
+                <p class="menu-item"><NuxtLink class="menu-text" to="/work">work</NuxtLink></p>
+                <p class="menu-item"><NuxtLink class="menu-text" to="/art">art</NuxtLink></p>
             </div>
         </div>
     </nav>
@@ -16,29 +16,43 @@
 
 <style>
     .title{
-        margin-top: -3.6em;
+        margin-top: -2.6em;
     }
     .logo {
-        font-size: 4.6em;
+        font-size: 3em;
+    }
+    .logo-text {
+        color: #ff2830;
+        text-decoration: none;
+    }
+    .logo-text:hover {
+        color: #ff282fbb;
     }
     .wrapper{ 
-        background-color: #ff2830;
         display: flex;
         flex-direction: column;
         align-items: center;
         margin: 0;
+        background: white;
+        width: 25vw;
     }
     .menu{
         display: flex;
         flex-direction: row;
-        margin-top: -2em;
+        margin-top: -3.4em;
         height: 3.2em;
         gap: 2em;
     }
-    .menu-item{
+    .menu-text{
+        color: #ff2830;
         text-decoration: none;
     }
-    .menu-item:hover{
-        color: white;
+    .menu-text:hover {
+        text-decoration: underline;
+    }
+    nav {
+        position: fixed;
+        background: white;
+        height: 100vh;
     }
 </style>
