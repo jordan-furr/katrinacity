1e7879f0e387461813869719931d25c57cfef0ba
jordanfurr <ejfurr@umich.edu>
2024-02-09 12:05:18 -0500

    scroll facts

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,45 @@
 <template>
-    <div class="h-92 flex">
-        <div class="main-section ph3 pb3 w-100">
+    <div class="h-92 flex flex-row">
+        <div class="main-section pl3 pb3 w-80">
             <div class="grid overflow-y h-100">
-                <div class="home-content pa5">
-                    <div class="item w-30 facts">
-                        <div class="w-100 pa4 bg-purple">
-                            <p>Katrina Stalcup is an artist and professional based in Brooklyn, NY.</p>
-                            <br>
-                            <p> Katrina's favorite color is purple. Katrina likes to eat take out from Little
-                                Tiffin. Katrina is
-                                currently reading The Gastronomical Me by M.F.K. Fisher.</p>
-                            <br>
-                            <p>CONTACT</p>
-                            <p>email: kat.stalcup@gmail.com</p>
-                            <p><a href="https://www.instagram.com/katrinacity/" class="under-hov purple-text">instagram</a></p>
+                <div class="pa5 relative">
+                    <div class="home-content">
+                        <div class="flex flex-row">
+                            <div class="item">
+                                <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                                <p>e</p>
+                            </div>
+                            <div class="item">
+                                <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                                <p>e</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                            <p>e</p>
+                        </div>
+                        <div class="item">
+                            <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                            <p>e</p>
+                        </div>
+                        <div class="item">
+                            <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                            <p>e</p>
+                        </div>
+                        <div class="item">
+                            <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                            <p>e</p>
                         </div>
                     </div>
-                    <div class="item">
-                        <img class="w-20" src="~/assets/img/flowers.jpeg" alt="">
-                        <p>e</p>
-                    </div>
-                    <div class="item">
-                        <img class="w-20" src="~/assets/img/flowers.jpeg" alt="">
-                        <p>e</p>
-                    </div>
-                    <div class="item">
-                        <img class="w-20" src="~/assets/img/flowers.jpeg" alt="">
-                        <p>e</p>
-                    </div>
                 </div>
+            </div>
+        </div>
+        <div class="info-section w-20 flex pr3 pb3">
+            <div class="w-100 pa4 facts bg-purple overflow-y">
+                <p>Katrina Stalcup is an artist and professional based in Brooklyn, NY.</p>
+                <br>
+                <p>CONTACT</p>
+                <p>kat.stalcup@gmail.com</p>
             </div>
         </div>
     </div>
---
+++ pages/work/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="h-92 flex flex-row">
+    <div class="main-section pl3 pb3 w-80">
+      <div class="grid overflow-y h-100">
+        <div class="pa5 relative">
+          <div class="home-content">
+            <div class="flex flex-row">
+              <div class="item">
+                <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                <p>e</p>
+              </div>
+              <div class="item">
+                <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+                <p>e</p>
+              </div>
+            </div>
+            <div class="item">
+              <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+              <p>e</p>
+            </div>
+            <div class="item">
+              <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+              <p>e</p>
+            </div>
+            <div class="item">
+              <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+              <p>e</p>
+            </div>
+            <div class="item">
+              <img class="w-30" src="~/assets/img/flowers.jpeg" alt="">
+              <p>e</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="info-section w-20 flex pr3 pb3">
+      <div class="w-100 pa4 facts bg-purple  overflow-y">
+        <p>Katrina Stalcup is an artist and professional based in Brooklyn, NY.</p>
+        <br>
+        <p>CONTACT</p>
+        <p>kat.stalcup@gmail.com</p>
+        <br>
+        <p>Katrina's favorite colors are purple, red, and blue.</p>
+        <br>
+        <p>Katrina likes to eat take out from Little Tiffin. Katrina is currently reading The Gastronomical Me by
+          M.F.K. Fisher.</p>
+        <br>
+        <p>Katrina graduated from UofM in 2020 with an honors degree in American Culture.</p>
+        <br>
+        <p>Katrina cares about meaning making and critical thinking.</p>
+        <br>
+        <p>Katrina likes the Twilight series.</p>
+        <br>
+        <p>Katrina was born on February 19th in Illinois.</p>
+        <br>
+        <p>Katrina likes to take long walks on the weekends.</p>
+        <br>
+        <p>Katrina's favorite bakery in New York is Radio Bakery, right around the corner.</p>
+        <br>
+        <p>Katrina's bagel order is an everything (or onion) bagel, toasted, with dill (or green onion) cream
+          cheese.</p>
+        <br>
+        <p>Katrina is a really great partner and friend.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
